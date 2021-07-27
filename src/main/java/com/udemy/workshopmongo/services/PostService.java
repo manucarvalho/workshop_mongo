f6cf851214bc7f42e3dd35d46fbe419d68ed020e
manucarvalho <manuela_carvalho_6@hotmail.com>
2021-07-27 15:41:27 -0300

    Comentários e consultas

---
+++ src/main/java/com/udemy/workshopmongo/services/PostService.java
@@ -0,0 +1,38 @@
+package com.udemy.workshopmongo.services;
+
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.workshopmongo.domain.Post;
+
+import com.udemy.workshopmongo.repository.PostRepository;
+
+import com.udemy.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	public PostRepository repo;
+	
+		
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}	
+	
+	public List<Post> findByTitle(String text){
+		//return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24*60*60*100);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
+}
